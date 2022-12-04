a92bbb35e64538fc2e718fa65344302bb3cd03ce
johannesrave <johannes.rave@gmail.com>
2022-12-04 17:17:06 +0100

    challenge-01-B solution

--- challenge-01/solution.mjs
+++ challenge-01/solution.mjs
@@ -1,4 +1,5 @@
 import fs from 'fs';
+
 const input = fs.readFileSync('input.txt', 'utf-8');
 
 function findHungriestElf(input) {
@@ -11,4 +12,18 @@ function findHungriestElf(input) {
     .reduce((max, cur) => max = (cur > max) ? cur : max, 0)
 }
 
+function findThreeHungriestElves(input) {
+  return input
+    .split('\n\n')
+    .map((e) => e
+      .split('\n')
+      .map(f => parseInt(f)))
+    .map(l => l.reduce((sum, cal) => sum + cal))
+    .reduce(
+      (max, cur) => (cur > max[0]) ? [cur, ...max].sort().slice(1, 4) : max,
+      [0, 0, 0])
+    .reduce((sum, cal) => sum + cal, 0);
+}
+
 console.log(findHungriestElf(input))
+console.log(findThreeHungriestElves(input))
