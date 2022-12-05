01d1660c99f4fbcc71daf0934773790e01e5cbbd
johannesrave <johannes.rave@gmail.com>
2022-12-06 00:25:28 +0100

    day-4 solved

---
+++ day-04/solution.mjs
@@ -0,0 +1,42 @@
+import fs from 'fs'
+
+const input = fs.readFileSync('input.txt', 'utf-8')
+
+function sumContainedSections(input) {
+    return input.split('\n')
+        .map((line) => line
+            .split(',')
+            .map(elves => elves
+                .split('-')
+                .map(n => parseInt(n, 10))), 0)
+        .reduce((sum, elves) =>
+            (eitherIsFullyContained(elves)) ? sum+1 : sum, 0)
+}
+
+console.log(sumContainedSections(input))
+
+function sumOverlappingSections(input) {
+    return input.split('\n')
+        .map((line) => line
+            .split(',')
+            .map(elves => elves
+                .split('-')
+                .map(n => parseInt(n, 10))), 0)
+        .reduce((sum, elves) => {
+            console.log(elves)
+            console.log(bothAreOverlapping(elves))
+            return (bothAreOverlapping(elves) || eitherIsFullyContained(elves)) ? sum + 1 : sum;
+        }, 0)
+}
+
+console.log(sumOverlappingSections(input))
+
+function eitherIsFullyContained(elves) {
+    return (elves[0][0] >= elves[1][0] && elves[0][1] <= elves[1][1]) ||
+        (elves[0][0] <= elves[1][0] && elves[0][1] >= elves[1][1]);
+}
+
+function bothAreOverlapping(elves) {
+    return (elves[0][0] >= elves[1][0] && elves[0][0] <= elves[1][1]) ||
+        (elves[0][1] <= elves[1][1] && elves[0][1] >= elves[1][0]);
+}
