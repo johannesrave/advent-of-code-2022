9605c40662f015f2a5c918c65e11e3b617c47557
johannesrave <johannes.rave@gmail.com>
2022-12-08 15:07:05 +0100

    day-6-b solved

--- day-06/solution.mjs
+++ day-06/solution.mjs
@@ -2,14 +2,24 @@ import fs from 'fs'
 
 const input = fs.readFileSync('input.txt', 'utf-8')
 
-function findBeginMarker(input) {
+function findStartOfPacketMarker(input) {
     return [...input].findIndex((symbol, i, arr) => {
         return new Set(arr.slice(i, i + 4)).size === 4
     }) + 4
 }
 
-console.log(findBeginMarker(input))
+function findStartOfMessageParker(input) {
+    return [...input].findIndex((symbol, i, arr) => {
+        return new Set(arr.slice(i, i + 14)).size === 14
+    }) + 14
+}
+
+console.log(findStartOfPacketMarker(input))
+console.log(findStartOfMessageParker(input))
 
 
-console.assert(findBeginMarker("bvwbjplbgvbhsrlpgdmjqwftvncz") === 5)
-console.assert(findBeginMarker("nppdvjthqldpwncqszvftbrmjlhg") === 6)
+console.assert(findStartOfPacketMarker("bvwbjplbgvbhsrlpgdmjqwftvncz") === 5)
+console.assert(findStartOfPacketMarker("nppdvjthqldpwncqszvftbrmjlhg") === 6)
+
+console.assert(findStartOfMessageParker("mjqjpqmgbljsphdztnvjfqwrcgsmlb") === 19)
+console.assert(findStartOfMessageParker("bvwbjplbgvbhsrlpgdmjqwftvncz") === 23)
