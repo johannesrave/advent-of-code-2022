4a486b57c31dc8abff9a995092a7d5bd68d15553
johannesrave <johannes.rave@gmail.com>
2022-12-12 12:33:04 +0100

    day-10-b solved

--- day-10/solution.ts
+++ day-10/solution.ts
@@ -2,27 +2,62 @@ import * as fs from 'fs';
 
 const testInput = fs.readFileSync('test_input.txt', 'utf-8')
 console.assert(calculateSignalStrength(testInput) === 13140)
+console.assert(renderCRT(testInput) ===
+"##..##..##..##..##..##..##..##..##..##..\n" +
+"###...###...###...###...###...###...###.\n" +
+"####....####....####....####....####....\n" +
+"#####.....#####.....#####.....#####.....\n" +
+"######......######......######......####\n" +
+"#######.......#######.......#######....."
+)
+console.log(renderCRT(testInput))
 
 const input = fs.readFileSync('input.txt', 'utf-8')
 console.log(calculateSignalStrength(input))
+console.log(renderCRT(input))
+// EPJBRKAH
+
 
 function calculateSignalStrength(input: string) {
-    const commands = input.split('\n');
+    const commands = input.split('\n')
     let signalStrength = 0
     let addReg = 0
-    let xReg = 1;
+    let xReg = 1
     let processing = false;
-    let cycle = 1;
+    let cycle = 1
     while (cycle <= 220) {
-        // console.log({cycle})
         if ((cycle - 20) % 40 === 0) {
             signalStrength += (cycle * xReg)
-            // console.log({cycle, xReg, addedSignalStrength: cycle * xReg, signalStrength})
         }
-        cycle++;
+        cycle++
         if (processing) {
             xReg += addReg
-            // console.log({xReg, addReg})
+            addReg = 0
+            processing = false
+            continue
+        }
+        const command = commands.shift()
+        if (command.startsWith('addx')) {
+            addReg = parseInt(command.split(' ')[1])
+            processing = true
+        }
+    }
+    return signalStrength
+
+}
+
+function renderCRT(input: string) {
+    const commands = input.split('\n')
+    let addReg = 0
+    let xReg = 1
+    let processing = false
+    let cycle = 1
+    let CRT = []
+    while (cycle <= 240) {
+        CRT = updateCRT(cycle, xReg+1, CRT)
+        cycle++
+        if (processing) {
+            xReg += addReg
             addReg = 0
             processing = false
             continue
@@ -30,11 +65,26 @@ function calculateSignalStrength(input: string) {
 
         const command = commands.shift()
         if (command.startsWith('addx')) {
-            // console.log({command: command.split(' ')})
             addReg = parseInt(command.split(' ')[1])
             processing = true
         }
     }
-    return signalStrength;
 
+    const CRTRows = []
+    for (let i = 0; i < CRT.length; i += 40) {
+        const chunk = CRT.slice(i, i + 40)
+        CRTRows.push(chunk)
+    }
+    const img = CRTRows.map(row => row.join('')).join('\n')
+    return img
+}
+
+function updateCRT(cycle: number, xReg: number, CRT: string[]) {
+    const pixels = [xReg - 1, xReg, xReg + 1]
+    if (pixels.includes(cycle % 40)) {
+        CRT.push('#')
+    } else {
+        CRT.push('.')
+    }
+    return CRT
 }
