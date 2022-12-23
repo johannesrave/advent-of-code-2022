8324b6b1b2634ef2300ee78e8cd5bd1047557f5e
johannesrave <johannes.rave@gmail.com>
2022-12-23 17:26:10 +0100

    day-13-b solved

--- day-13/solution.ts
+++ day-13/solution.ts
@@ -3,6 +3,12 @@ import * as fs from 'fs';
 const testInput = fs.readFileSync('test_input.txt', 'utf-8')
 const input = fs.readFileSync('input.txt', 'utf-8')
 
+console.assert(addUpIndicesOfOrderedPairs(testInput) === 13)
+console.log(addUpIndicesOfOrderedPairs(input)) // 6656
+
+console.assert(calculateDecoderKey(testInput) === 140)
+console.log(calculateDecoderKey(input)) // 19716
+
 function addUpIndicesOfOrderedPairs(input: string) {
     const pairs = input
         .split('\n\n')
@@ -16,14 +22,26 @@ function addUpIndicesOfOrderedPairs(input: string) {
     return sum
 }
 
+function calculateDecoderKey(input: string) {
+    const packages = input
+        .split('\n\n')
+        .flatMap(pair => pair
+            .split('\n').map(arr => eval(arr))) as [any[], any[]][]
+
+    const divA = [[2]];
+    const divB = [[6]];
+    const sortedList = [...packages, divA, divB].sort((l, r) => compare(l, r)).reverse()
+    return (sortedList.indexOf(divA)+1) * (sortedList.indexOf(divB)+1)
+}
+
 function isOrdered(pair: [any[], any[]]) {
     const [left, right] = pair
     for (const j of left.keys()) {
         const _isOrdered = compare(left, right)
         switch (_isOrdered) {
-            case 'ordered':
+            case 1:
                 return true
-            case 'unordered':
+            case -1:
                 return false
         }
     }
@@ -32,14 +50,14 @@ function isOrdered(pair: [any[], any[]]) {
 
 function compare(a, b) {
     if (typeof a === 'undefined' && typeof b !== 'undefined') {
-        return 'ordered'
+        return 1
     } else if (typeof b === 'undefined') {
-        return 'unordered'
+        return -1
     } else if (typeof a === 'number' && typeof b === 'number') {
         if (a < b) {
-            return 'ordered'
+            return 1
         } else if (a > b) {
-            return 'unordered'
+            return -1
         }
     } else if (typeof a === 'object' && typeof b === 'number') {
         return compare(a, [b])
@@ -47,43 +65,39 @@ function compare(a, b) {
         return compare([a], b)
     } else if (typeof a === 'object' && typeof b === 'object') {
         if (a.length === 0 && b.length > 0) {
-            return 'ordered'
+            return 1
         } else if (a.length > 0 && b.length === 0) {
-            return 'unordered'
+            return -1
         } else if (a.length === 0 && b.length === 0) {
-            return 'inconclusive'
+            return 0
         }
 
         for (let i = 0; i < Math.max(a.length, b.length); i++) {
             const res = compare(a[i], b[i])
-            if (res !== 'inconclusive') {
+            if (res !== 0) {
                 return res
             }
         }
     }
-    return 'inconclusive'
+    return 0
 }
 
-console.assert(compare(1, 1) === 'inconclusive')
-console.assert(compare(1, 2) === 'ordered')
-console.assert(compare(2, 1) === 'unordered')
-console.assert(compare(1, undefined) === 'unordered')
-console.assert(compare(1, [2]) === 'ordered')
-console.assert(compare([2], 1) === 'unordered')
-console.assert(compare([2], []) === 'unordered')
-console.assert(compare([], [2]) === 'ordered')
-console.assert(compare([2], undefined) === 'unordered')
-console.assert(compare([2], [2]) === 'inconclusive')
-console.assert(compare([[1]], [[2]]) === 'ordered')
-console.assert(compare([[2]], [[1]]) === 'unordered')
-console.assert(compare([[2]], [[]]) === 'unordered')
-console.assert(compare([[2]], [[2]]) === 'inconclusive')
-console.assert(compare([], []) === 'inconclusive')
-console.assert(compare([], [[]]) === 'ordered')
-console.assert(compare([], [3]) === 'ordered')
-console.assert(compare([[]], []) === 'unordered')
-console.assert(compare([[]], [[]]) === 'inconclusive')
-
-console.assert(addUpIndicesOfOrderedPairs(testInput) === 13)
-console.log(addUpIndicesOfOrderedPairs(testInput))
-console.log(addUpIndicesOfOrderedPairs(input))
+console.assert(compare(1, 1) === 0)
+console.assert(compare(1, 2) === 1)
+console.assert(compare(2, 1) === -1)
+console.assert(compare(1, undefined) === -1)
+console.assert(compare(1, [2]) === 1)
+console.assert(compare([2], 1) === -1)
+console.assert(compare([2], []) === -1)
+console.assert(compare([], [2]) === 1)
+console.assert(compare([2], undefined) === -1)
+console.assert(compare([2], [2]) === 0)
+console.assert(compare([[1]], [[2]]) === 1)
+console.assert(compare([[2]], [[1]]) === -1)
+console.assert(compare([[2]], [[]]) === -1)
+console.assert(compare([[2]], [[2]]) === 0)
+console.assert(compare([], []) === 0)
+console.assert(compare([], [[]]) === 1)
+console.assert(compare([], [3]) === 1)
+console.assert(compare([[]], []) === -1)
+console.assert(compare([[]], [[]]) === 0)
