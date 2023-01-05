5f8d6539c1e4a9bb1f0150af26b61d75d0f661da
johannesrave <johannes.rave@gmail.com>
2023-01-06 00:50:16 +0100

    day-15-b solved

--- day-15/solution.ts
+++ day-15/solution.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 const testInput = fs.readFileSync('test_input.txt', 'utf-8');
 const input = fs.readFileSync('input.txt', 'utf-8');
 
-function work(input, rowToScan: number) {
+function findScannedTilesOnRow(input, rowToScan: number) {
     const knownTiles = input.split('\n').map(line => {
         const [, sensorX, sensorY, beaconX, beaconY] = line
             .match(/Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/)
@@ -43,8 +43,46 @@ function work(input, rowToScan: number) {
     return tilesInRange - beaconsOnScannedRow.length
 }
 
-console.log(work(testInput, 10))
-console.assert(work(testInput, 10) === 26)
+function findMissingBeaconInRange(input, bounds: number) {
+    const knownTiles = input.split('\n').map(line => {
+        const [, sensorX, sensorY, beaconX, beaconY] = line
+            .match(/Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/)
+        const range = Math.abs(sensorX - beaconX) + Math.abs(sensorY - beaconY)
+        return {
+            sensor: {x: parseInt(sensorX), y: parseInt(sensorY), range},
+            beacon: {x: parseInt(beaconX), y: parseInt(beaconY)}
+        }
+    })
 
-console.log(work(input, 2000_000))
-// 6263574 is too high
+    const sensors: { x: number, y: number, range: number }[] = knownTiles
+        .map(tile => tile.sensor)
+
+    let missingBeacon = [-1, -1]
+    search: for (let y = 0; y < bounds; y++) {
+        for (let x = 0; x <= bounds; x++) {
+            const additionalRanges = sensors
+                .map(s => {
+                    return s.range - (Math.abs(s.y - y) + Math.abs(s.x - x))
+                })
+                .filter(range => range >= 0)
+
+            if (additionalRanges.length > 0) {
+                x += Math.max(...additionalRanges)
+            } else {
+                missingBeacon = [x, y]
+                break search
+            }
+        }
+        if (y % 100_000 === 0) console.log(y)
+    }
+
+    const [x, y] = missingBeacon
+    return x * 4_000_000 + y
+}
+
+console.assert(findScannedTilesOnRow(testInput, 10) === 26)
+console.log(findScannedTilesOnRow(input, 2_000_000))
+console.assert(findScannedTilesOnRow(input, 2_000_000) === 5525990)
+
+console.assert(findMissingBeaconInRange(testInput, 20) === 56000011)
+console.log(findMissingBeaconInRange(input, 4_000_000))
